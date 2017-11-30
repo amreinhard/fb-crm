6e88aed35c7f2e4eb9cf537f939dd7fed6056515
Amanda <amreinhard@icloud.com>
2017-11-30 03:53:16 +0000

    Hammering out bugs.

--- facebook-parser.py
+++ facebook-parser.py
@@ -2,6 +2,7 @@
 import re, os
 from HTMLParser import HTMLParser
 from pprint import pprint
+from pythonds.basic.stack import Stack
 
 with open('friends.htm', 'r') as f: #closes automatically, handles any exceptions
     reading = f.read()
@@ -10,77 +11,59 @@ pyfile = os.path.dirname(os.path.abspath(__file__)) #wherever you go, there you 
 
 class FriendParser(HTMLParser):
 
-    stack = []
+    s = Stack()
+    friends = []
     current_head = ""
     data = ""
     header = ['html', 'body', 'div', 'h2']
     expected = ['html', 'body', 'div', 'ul', 'li']
-    friends = []
 
     def handle_starttag(self, tag, attrs):
-        self.stack.append(tag)
-        if self.stack == self.expected:
+        self.s.push(tag)
+        if self.s == self.expected:
             self.data = ""
 
     def handle_endtag(self, tag):
-        if self.stack == self.expected:
+        if self.s == self.expected:
             self.friends.append(Friend(self.data, self.current_head))
-        self.stack.pop()
+        self.s.pop()
 
     def handle_data(self, data):
-        if self.stack == self.expected:
+        if self.s == self.expected:
             self.data += data
-        if self.stack == self.header:
+        if self.s == self.header:
             self.current_head = data
 
     def handle_entityref(self, name):
-        if self.stack == self.expected:
+        if self.s == self.expected:
             self.data += name
 
     def handle_charref(self, name):
-        if self.stack == self.expected:
-            self.data += unichr(name) #hexadecimal bug - strip/map?
+        if self.s == self.expected:
+            self.data += name #hexadecimal bug - unichr/chr? - strip/map?
 
-    #how do I map regex to str and get the groups back? name and date. match regex to str to get matches back
 
 class Friend():
 
     name = ""
     status = ""
     date = ""
+    parens = r"([^\(]+)\(([^)]+)\)"
 
     def __init__(self, name, status):
         self.name, self.date = self._parse_name(name)
         self.status = status
 
     def _parse_name(self, name):
-        #([^\(]+)\(([^)]+)\)
-        return (name, "")
+        clean = re.findall(self.parens)
+        print name, clean
+        return (name, clean)
 
     def __str__(self):
-        return "Friend(%s, %s)" % (self.name, self.status) #add status later
+        return "Friend(%s, %s)" % (self.name, self.status)  # add status later
 
-def message_name():
-    """Finds names within message files."""
-    idname = {}
-
-    for subdir, dirs, files in os.walk(pyfile + '/messages'):
-        for filename in files:
-            message_open = open(filename, 'r')
-            message_read = message_open.read()
-            message_open.close()
-            #how do I read from <title> brackets in every file?
-            if filename not in idname:
-                idname[filename[:-5]] = None
-
-    pprint(idname)
-    return idname
-
-def id_association():
-    """Associates names to IDs via dictionary."""
 
 parser = FriendParser()
 parser.feed(reading)
 for friend in parser.friends:
     print friend
-message_name()
